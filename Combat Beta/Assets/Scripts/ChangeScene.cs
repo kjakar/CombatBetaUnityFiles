868a222323c6349bb328510bc2e613499a7193d5
kjakar <kjakarproductions@gmail.com>
2016-11-27 03:41:00 -0500

    Change Scenes
    I added a script to change scenes, as well as a scene running that
    script as a main menu of sorts for testing throughout the dev process.

---
+++ Combat Beta/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	
+	public void SwitchScenes(int scene)
+    {
+        SceneManager.LoadScene(scene);
+	}
+}
